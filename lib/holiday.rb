f38992e2cc4b28f6e11ad1ee46216e7d12b32ec8
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-25 14:42:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,9 +63,9 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday, supply_list|
       puts "#{holiday.to_s.capitalize}: #{supply_list.to_s.capitalize}"
-      supply_list.each do |supply|
-        puts supply
-      end
+      #supply_list.each do |supply|
+        #puts supply
+      #end
     end
   end 
   
